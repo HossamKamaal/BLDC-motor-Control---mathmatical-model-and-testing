eac7fae94f53fbafcb822d7e28dcc805e2171f5c
HossamKamaal <hossammohamad142@gmail.com>
2025-06-02 03:23:47 +0300

    Add files via upload

---
+++ slprj/ert/BLDC_Controller1/sil/xil_interface.c
@@ -0,0 +1,429 @@
+/*
+ * File: xil_interface.c
+ *
+ * SIL generated interface for code: "BLDC_Controller1"
+ *
+ */
+
+#include "BLDC_Controller1.h"
+#include "xil_interface.h"
+
+/* Functions with a C call interface */
+#ifdef __cplusplus
+
+extern "C" {
+
+#endif
+
+#include "xil_data_stream.h"
+#ifdef __cplusplus
+
+}
+#endif
+
+/* interface data */
+boolean_T i_hall_signals[3];
+
+/* interface data */
+real_T i_Emf[3];
+
+/* interface data */
+boolean_T i_control_signal;
+
+/* interface data */
+boolean_T o_g1;
+
+/* interface data */
+boolean_T o_g2;
+
+/* interface data */
+boolean_T o_g3;
+
+/* interface data */
+boolean_T o_g4;
+
+/* interface data */
+boolean_T o_g5;
+
+/* interface data */
+boolean_T o_g6;
+
+/* interface data */
+BLDC_Controller1_MdlrefDW BLDC_Controller1rtMdlrefDW;
+static XILIOData xil_fcnid0_task1_output_u[4];
+static XILIOData xil_fcnid0_task1_y[7];
+
+/* In-the-Loop Interface functions - see xil_interface.h */
+XIL_INTERFACE_ERROR_CODE xilProcessParams(uint32_T xilFcnId)
+{
+  /* Single In-the-Loop Component */
+  if (xilFcnId != 0) {
+    return XIL_INTERFACE_UNKNOWN_FCNID;
+  }
+
+  return XIL_INTERFACE_SUCCESS;
+}
+
+XIL_INTERFACE_ERROR_CODE xilGetDataTypeInfo(void)
+{
+  {
+    /* send response id code */
+    MemUnit_T memUnitData = XIL_RESPONSE_TYPE_SIZE;
+    if (xilWriteData(&memUnitData, sizeof(memUnitData)) !=
+        XIL_DATA_STREAM_SUCCESS) {
+      return XIL_INTERFACE_COMMS_FAILURE;
+    }
+
+    /* send type id */
+    memUnitData = 0;
+    if (xilWriteData(&memUnitData, sizeof(memUnitData)) !=
+        XIL_DATA_STREAM_SUCCESS) {
+      return XIL_INTERFACE_COMMS_FAILURE;
+    }
+
+    /* PIL_DOUBLE_SIZE should only be already defined for MathWorks testing */
+#ifndef PIL_DOUBLE_SIZE
+#define PIL_DOUBLE_SIZE                sizeof(double)
+#endif
+
+    /* send size in bytes */
+    memUnitData = (MemUnit_T) PIL_DOUBLE_SIZE;
+
+#ifndef HOST_WORD_ADDRESSABLE_TESTING
+
+    /* convert MemUnits to bytes */
+    memUnitData *= MEM_UNIT_BYTES;
+
+#endif
+
+    if (xilWriteData(&memUnitData, sizeof(memUnitData)) !=
+        XIL_DATA_STREAM_SUCCESS) {
+      return XIL_INTERFACE_COMMS_FAILURE;
+    }
+  }
+
+  return XIL_INTERFACE_SUCCESS;
+}
+
+XIL_INTERFACE_ERROR_CODE xilInitialize(uint32_T xilFcnId)
+{
+  XIL_INTERFACE_ERROR_CODE errorCode = XIL_INTERFACE_SUCCESS;
+
+  /* initialize output storage owned by In-the-Loop */
+  {
+    o_g1 = 0;
+  }
+
+  {
+    o_g2 = 0;
+  }
+
+  {
+    o_g3 = 0;
+  }
+
+  {
+    o_g4 = 0;
+  }
+
+  {
+    o_g5 = 0;
+  }
+
+  {
+    o_g6 = 0;
+  }
+
+  /* Single In-the-Loop Component */
+  if (xilFcnId == 0) {
+    /* No Function to Call */
+    /* Call all Get class methods */
+  } else {
+    errorCode = XIL_INTERFACE_UNKNOWN_FCNID;
+  }
+
+  return errorCode;
+}
+
+XIL_INTERFACE_ERROR_CODE xilPause(uint32_T xilFcnId)
+{
+  XIL_INTERFACE_ERROR_CODE errorCode = XIL_INTERFACE_SUCCESS;
+  if (xilFcnId == 0) {
+    /* Nothing to do */
+  } else {
+    errorCode = XIL_INTERFACE_UNKNOWN_FCNID;
+  }                                    /* if */
+
+  return errorCode;
+}
+
+XIL_INTERFACE_ERROR_CODE xilSystemInitialize(uint32_T xilFcnId)
+{
+  XIL_INTERFACE_ERROR_CODE errorCode = XIL_INTERFACE_SUCCESS;
+
+  /* Single In-the-Loop Component */
+  if (xilFcnId == 0) {
+    /* No Function to Call */
+  } else {
+    errorCode = XIL_INTERFACE_UNKNOWN_FCNID;
+  }
+
+  return errorCode;
+}
+
+XIL_INTERFACE_ERROR_CODE xilSystemReset(uint32_T xilFcnId)
+{
+  XIL_INTERFACE_ERROR_CODE errorCode = XIL_INTERFACE_SUCCESS;
+
+  /* Single In-the-Loop Component */
+  if (xilFcnId == 0) {
+    /* No Function to Call */
+  } else {
+    errorCode = XIL_INTERFACE_UNKNOWN_FCNID;
+  }
+
+  return errorCode;
+}
+
+XIL_INTERFACE_ERROR_CODE xilGetHostToTargetData(uint32_T xilFcnId,
+  XIL_COMMAND_TYPE_ENUM xilCommandType, uint32_T xilCommandIdx, XILIOData
+  ** xilIOData)
+{
+  XIL_INTERFACE_ERROR_CODE errorCode = XIL_INTERFACE_SUCCESS;
+  *xilIOData = 0;
+
+  /* Single In-the-Loop Component */
+  if (xilFcnId != 0) {
+    errorCode = XIL_INTERFACE_UNKNOWN_FCNID;
+    return errorCode;
+  }
+
+  switch (xilCommandType) {
+   case XIL_OUTPUT_COMMAND:
+    {
+      static int initComplete = 0;
+      if (!initComplete) {
+        uint32_T tableIdx = 0;
+
+        {
+          void * dataAddress = (void *) &(i_hall_signals[0]);
+          xil_fcnid0_task1_output_u[tableIdx].memUnitLength = 3 * sizeof
+            (boolean_T);
+          xil_fcnid0_task1_output_u[tableIdx++].address = (MemUnit_T *)
+            dataAddress;
+        }
+
+        {
+          void * dataAddress = (void *) &(i_Emf[0]);
+          xil_fcnid0_task1_output_u[tableIdx].memUnitLength = 3 * sizeof(real_T);
+          xil_fcnid0_task1_output_u[tableIdx++].address = (MemUnit_T *)
+            dataAddress;
+        }
+
+        {
+          void * dataAddress = (void *) &(i_control_signal);
+          xil_fcnid0_task1_output_u[tableIdx].memUnitLength = 1 * sizeof
+            (boolean_T);
+          xil_fcnid0_task1_output_u[tableIdx++].address = (MemUnit_T *)
+            dataAddress;
+        }
+
+        xil_fcnid0_task1_output_u[tableIdx].memUnitLength = 0;
+        xil_fcnid0_task1_output_u[tableIdx++].address = (MemUnit_T *) 0;
+        initComplete = 1;
+      }                                /* if */
+
+      *xilIOData = &xil_fcnid0_task1_output_u[0];
+      break;
+    }
+
+   default:
+    errorCode = XIL_INTERFACE_UNKNOWN_TID;
+    break;
+  }
+
+  UNUSED_PARAMETER(xilCommandIdx);
+  return errorCode;
+}
+
+XIL_INTERFACE_ERROR_CODE xilGetTargetToHostPreData(uint32_T xilFcnId,
+  XIL_COMMAND_TYPE_ENUM xilCommandType, uint32_T xilCommandIdx, XILIOData
+  ** xilIOData, MemUnit_T responseId, uint32_T serverFcnId)
+{
+  XIL_INTERFACE_ERROR_CODE errorCode = XIL_INTERFACE_SUCCESS;
+  *xilIOData = 0;
+  if (xilFcnId != 0) {
+    errorCode = XIL_INTERFACE_UNKNOWN_FCNID;
+    return errorCode;
+  }                                    /* if */
+
+  errorCode = XIL_INTERFACE_UNKNOWN_TID;
+  UNUSED_PARAMETER(xilCommandType);
+  UNUSED_PARAMETER(xilCommandIdx);
+  UNUSED_PARAMETER(responseId);
+  UNUSED_PARAMETER(serverFcnId);
+  return errorCode;
+}
+
+XIL_INTERFACE_ERROR_CODE xilOutput(uint32_T xilFcnId, uint32_T xilTID)
+{
+  /* Single In-the-Loop Component */
+  if (xilFcnId != 0) {
+    return XIL_INTERFACE_UNKNOWN_FCNID;
+  }
+
+  switch (xilTID) {
+   case 1:
+    /* Call all Set class methods */
+    BLDC_Controller1(i_hall_signals, i_Emf, &(i_control_signal), &(o_g1), &(o_g2),
+                     &(o_g3), &(o_g4), &(o_g5), &(o_g6),
+                     &(BLDC_Controller1rtMdlrefDW.rtdw));
+
+    /* Call all Get class methods */
+    break;
+
+   default:
+    return XIL_INTERFACE_UNKNOWN_TID;
+  }
+
+  return XIL_INTERFACE_SUCCESS;
+}
+
+XIL_INTERFACE_ERROR_CODE xilUpdate(uint32_T xilFcnId, uint32_T xilTID)
+{
+  /* Single In-the-Loop Component */
+  if (xilFcnId != 0) {
+    return XIL_INTERFACE_UNKNOWN_FCNID;
+  }
+
+  /* No Update Function */
+  UNUSED_PARAMETER(xilTID);
+  return XIL_INTERFACE_SUCCESS;
+}
+
+XIL_INTERFACE_ERROR_CODE xilGetTargetToHostData(uint32_T xilFcnId,
+  XIL_COMMAND_TYPE_ENUM xilCommandType, uint32_T xilCommandIdx, XILIOData
+  ** xilIOData, MemUnit_T responseId, uint32_T serverFcnId)
+{
+  XIL_INTERFACE_ERROR_CODE errorCode = XIL_INTERFACE_SUCCESS;
+
+  /* Single In-the-Loop Component */
+  *xilIOData = 0;
+  if (xilFcnId != 0) {
+    errorCode = XIL_INTERFACE_UNKNOWN_FCNID;
+    return errorCode;
+  }
+
+  switch (xilCommandType) {
+   case XIL_OUTPUT_COMMAND:
+    {
+      static int initComplete = 0;
+      if (!initComplete) {
+        uint32_T tableIdx = 0;
+
+        {
+          void * dataAddress = (void *) &(o_g1);
+          xil_fcnid0_task1_y[tableIdx].memUnitLength = 1 * sizeof(boolean_T);
+          xil_fcnid0_task1_y[tableIdx++].address = (MemUnit_T *) dataAddress;
+        }
+
+        {
+          void * dataAddress = (void *) &(o_g2);
+          xil_fcnid0_task1_y[tableIdx].memUnitLength = 1 * sizeof(boolean_T);
+          xil_fcnid0_task1_y[tableIdx++].address = (MemUnit_T *) dataAddress;
+        }
+
+        {
+          void * dataAddress = (void *) &(o_g3);
+          xil_fcnid0_task1_y[tableIdx].memUnitLength = 1 * sizeof(boolean_T);
+          xil_fcnid0_task1_y[tableIdx++].address = (MemUnit_T *) dataAddress;
+        }
+
+        {
+          void * dataAddress = (void *) &(o_g4);
+          xil_fcnid0_task1_y[tableIdx].memUnitLength = 1 * sizeof(boolean_T);
+          xil_fcnid0_task1_y[tableIdx++].address = (MemUnit_T *) dataAddress;
+        }
+
+        {
+          void * dataAddress = (void *) &(o_g5);
+          xil_fcnid0_task1_y[tableIdx].memUnitLength = 1 * sizeof(boolean_T);
+          xil_fcnid0_task1_y[tableIdx++].address = (MemUnit_T *) dataAddress;
+        }
+
+        {
+          void * dataAddress = (void *) &(o_g6);
+          xil_fcnid0_task1_y[tableIdx].memUnitLength = 1 * sizeof(boolean_T);
+          xil_fcnid0_task1_y[tableIdx++].address = (MemUnit_T *) dataAddress;
+        }
+
+        xil_fcnid0_task1_y[tableIdx].memUnitLength = 0;
+        xil_fcnid0_task1_y[tableIdx++].address = (MemUnit_T *) 0;
+        initComplete = 1;
+      }                                /* if */
+
+      {
+        if (xilWriteData(&responseId, sizeof(responseId)) !=
+            XIL_DATA_STREAM_SUCCESS) {
+          return XIL_INTERFACE_COMMS_FAILURE;
+        }                              /* if */
+
+        if (responseId == XIL_RESPONSE_CS_REQUEST_SERVICE) {
+          if (xilWriteData((MemUnit_T *) &serverFcnId, sizeof(serverFcnId)) !=
+              XIL_DATA_STREAM_SUCCESS) {
+            return XIL_INTERFACE_COMMS_FAILURE;
+          }                            /* if */
+        }                              /* if */
+      }
+
+      *xilIOData = &xil_fcnid0_task1_y[0];
+      break;
+    }
+
+   default:
+    errorCode = XIL_INTERFACE_UNKNOWN_TID;
+    break;
+  }
+
+  UNUSED_PARAMETER(xilCommandIdx);
+  UNUSED_PARAMETER(responseId);
+  UNUSED_PARAMETER(serverFcnId);
+  return errorCode;
+}
+
+XIL_INTERFACE_ERROR_CODE xilTerminate(uint32_T xilFcnId)
+{
+  if (xilFcnId != 0) {
+    return XIL_INTERFACE_UNKNOWN_FCNID;
+  }                                    /* if */
+
+  /* Invoke any terminate Function */
+  /* No Function to Call */
+  return XIL_INTERFACE_SUCCESS;
+}
+
+XIL_INTERFACE_ERROR_CODE xilEnable(uint32_T xilFcnId, uint32_T xilTID)
+{
+  /* Single In-the-Loop Component */
+  if (xilFcnId != 0) {
+    return XIL_INTERFACE_UNKNOWN_FCNID;
+  }
+
+  UNUSED_PARAMETER(xilTID);
+
+  /* No Enable Function - this function should never be called */
+  return XIL_INTERFACE_UNKNOWN_TID;
+}
+
+XIL_INTERFACE_ERROR_CODE xilDisable(uint32_T xilFcnId, uint32_T xilTID)
+{
+  /* Single In-the-Loop Component */
+  if (xilFcnId != 0) {
+    return XIL_INTERFACE_UNKNOWN_FCNID;
+  }
+
+  UNUSED_PARAMETER(xilTID);
+
+  /* No Disable Function - this function should never be called */
+  return XIL_INTERFACE_UNKNOWN_TID;
+}
